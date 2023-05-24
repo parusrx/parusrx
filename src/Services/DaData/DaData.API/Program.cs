1642c97d2dc85d31878868bec92f15589e8cf6fd
Alexander Bocharov <alex.bocharov@outlook.com>
2023-05-24 16:25:11 +0300

    chore: minimize docker image size for DaData.API

--- src/Services/DaData/DaData.API/Program.cs
+++ src/Services/DaData/DaData.API/Program.cs
@@ -1,35 +1,73 @@
 // Copyright (c) Alexander Bocharov. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateLogger();
+using Dapr;
+using Evolve.Data.Oracle;
+using Evolve.Data.PostgreSQL;
 
-Log.Information("Starting up...");
+var builder = WebApplication.CreateSlimBuilder(args);
 
-try
+builder.Services.AddDaprClient();
+
+builder.Services.Configure<DaDataSettings>(builder.Configuration.GetSection("URLs"));
+builder.Services.AddScoped<IEventBus, DaprEventBus>();
+
+// Application specified services
+builder.Services.AddHttpClient<ISuggestPartyService, SuggestPartyService>();
+builder.Services.AddScoped<ISuggestPartyIntegrationEventService, SuggestPartyIntegrationEventService>();
+builder.Services.AddTransient<SuggestPartyIntegrationEventHandler>();
+
+// Data access
+var provider = builder.Configuration["Database:Provider"];
+switch (provider)
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Host.UseSerilog((ctx, lc) => lc
-        .ReadFrom.Configuration(ctx.Configuration)
-        .Enrich.FromLogContext()
-        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"));
-    
-    var app = builder
-        .ConfigureServices()
-        .ConfigurePipeline();
-    
-    app.Run();
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Host terminated unexpectedly.");
-}
-finally
-{
-    Log.Information("Shutting down.");
-    Log.CloseAndFlush();
+    case "Oracle":
+#pragma warning disable CS8604 // Possible null reference argument.
+        builder.Services
+            .AddEvolveDataAccess(options => options.UseOracle(builder.Configuration["Database:ConnectionString"]))
+            .AddOracleParusRxStores();
+#pragma warning restore CS8604 // Possible null reference argument.
+        break;
+    case "Postgres":
+#pragma warning disable CS8604 // Possible null reference argument.
+        builder.Services
+            .AddEvolveDataAccess(options => options.UsePostgreSql(builder.Configuration["Database:ConnectionString"]))
+            .AddPostgresParusRxStore();
+#pragma warning restore CS8604 // Possible null reference argument.
+        break;
+    default:
+        throw new NotSupportedException($"Database provider {provider} is not supported.");
 }
 
-public partial class Program { }
+// CORS
+const string corsPolicy = "CorsPolicy";
+builder.Services.AddCors(options =>
+            options.AddPolicy(name: corsPolicy,
+                corsPolicyBuilder =>
+                {
+                    corsPolicyBuilder.SetIsOriginAllowed((host) => true);
+                    corsPolicyBuilder.AllowAnyMethod();
+                    corsPolicyBuilder.AllowAnyHeader();
+                    corsPolicyBuilder.AllowCredentials();
+                }));
+
+// Health Checks
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy());
+
+var app = builder.Build();
+
+app.UseCloudEvents();
+app.MapSubscribeHandler();
+
+app.MapHealthChecks("/health", new HealthCheckOptions { Predicate = _ => true });
+app.MapHealthChecks("/liveness", new HealthCheckOptions { Predicate = r => r.Name.Contains("self") });
+
+app.MapPost("/api/v1/suggestions", [Topic("pubsub", "DaDataSuggestionsFindByIdPartyIntegrationEvent")] async (IntegrationEvent @event, IServiceProvider serviceProvider) =>
+{
+    var handler = serviceProvider.GetRequiredService<SuggestPartyIntegrationEventHandler>();
+    await handler.HandleAsync(@event);
+    return Results.Ok();
+});
+
+app.Run();
--- src/Services/DaData/DaData.API/Usings.cs
+++ src/Services/DaData/DaData.API/Usings.cs
@@ -1,16 +1,15 @@
 // Copyright (c) Alexander Bocharov. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-global using Serilog;
-global using System.Reflection;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Xml.Serialization;
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 global using Microsoft.Extensions.Diagnostics.HealthChecks;
 global using Microsoft.Extensions.Options;
-global using Microsoft.OpenApi.Models;
 global using ParusRx.DaData.API;
+global using ParusRx.DaData.API.Integration;
+global using ParusRx.DaData.API.Integration.Handlers;
 global using ParusRx.DaData.API.Models;
 global using ParusRx.DaData.API.Services;
 global using ParusRx.EventBus;
