72031e06c130ea5703f4d20168de8edfa23cac16
Alexander Bocharov <alex.bocharov@outlook.com>
2023-06-09 17:01:46 +0300

    chore: refactoring code for EmployeeRole service

--- src/Services/HRLink.EmployeeRole/HRLink.EmployeeRole.API/Program.cs
+++ src/Services/HRLink.EmployeeRole/HRLink.EmployeeRole.API/Program.cs
@@ -1,37 +1,47 @@
 ﻿// Copyright (c) Alexander Bocharov. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information..
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+using Dapr;
 
 using Evolve.Data.Oracle;
 using Evolve.Data.PostgreSQL;
 
-using ParusRx.HRLink.EmployeeRole.API;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+using ParusRx.EventBus.Events;
+using ParusRx.Storage;
+using ParusRx.Xml;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(0, EmployeeRoleJsonSerializerContext.Default));
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
 
 builder.Services.AddDaprClient();
 builder.Services.AddDaprEventBus();
 
-builder.Services.AddEmployeeRoleServices();
+builder.Services.AddHttpClient();
 
 // Data access
-var provider = builder.Configuration["Database:Provider"];
+string provider = builder.Configuration["Database:Provider"] ?? string.Empty;
+string connectionString = builder.Configuration["Database:ConnectionString"] ?? string.Empty;
 switch (provider)
 {
     case "Oracle":
-#pragma warning disable CS8604 // Possible null reference argument.
         builder.Services
-            .AddEvolveDataAccess(options => options.UseOracle(builder.Configuration["Database:ConnectionString"]))
+            .AddEvolveDataAccess(options => options.UseOracle(connectionString))
             .AddOracleParusRxStores();
-#pragma warning restore CS8604 // Possible null reference argument.
         break;
     case "Postgres":
-#pragma warning disable CS8604 // Possible null reference argument.
         builder.Services
-            .AddEvolveDataAccess(options => options.UsePostgreSql(builder.Configuration["Database:ConnectionString"]))
+            .AddEvolveDataAccess(options => options.UsePostgreSql(connectionString))
             .AddPostgresParusRxStore();
-#pragma warning restore CS8604 // Possible null reference argument.
         break;
     default:
         throw new NotSupportedException($"Database provider {provider} is not supported.");
@@ -48,6 +58,168 @@ app.MapSubscribeHandler();
 app.MapHealthChecks("/health", new HealthCheckOptions { Predicate = _ => true});
 app.MapHealthChecks("/liveness", new HealthCheckOptions { Predicate = r => r.Name.Contains("self") });
 
-app.MapEmployeeRoles();
+app.MapPost("api/v1/employeeRoles", [Topic("pubsub", "EmployeeRolesIntegrationEvent")] async (MqIntegrationEvent @event, IHttpClientFactory httpClientFactory, IParusRxStore store) =>
+{
+    app.Logger.LogInformation("Handling integration event: {IntegrationEventId} - {IntegrationEvent}", @event.Id, @event);
+
+    string id = @event.Body;
+    try
+    {
+        byte[] data = await store.ReadDataRequestAsync(id);
+        var employeeRolesRequest = XmlSerializerUtility.Deserialize<EmployeeRolesRequest>(data);
+        if (employeeRolesRequest is not null)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            httpClient.BaseAddress = new Uri(employeeRolesRequest.Url);
+            httpClient.DefaultRequestHeaders.Add("User-Api-Token", employeeRolesRequest.ApiToken);
+
+            var employeeRolesResponse = await httpClient.GetFromJsonAsync("api/v1/employeeRoles", AppJsonSerializerContext.Default.EmployeeRolesResponse);
+            var employeeRolesDto = employeeRolesResponse?.EmployeeRoles?.AsEmployeeRolesDto();
+            if (employeeRolesDto is not null)
+            {
+                byte[]? response = XmlSerializerUtility.Serialize(employeeRolesDto);
+                if (response is not null)
+                {
+                    await store.SaveDataResponseAsync(id, response);
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        await store.ErrorAsync(id, ex.Message);
+        app.Logger.LogError(ex, "Error while processing integration event {IntegrationEventId} of type {IntegrationEventType}: {Message}", @event.Id, @event.GetType().Name, ex.Message);
+    }
+
+    return TypedResults.Created();
+});
 
 app.Run();
+
+/// <summary>
+/// The employee role item.
+/// </summary>
+/// <param name="Id">The employee role identifier.</param>
+/// <param name="Name">The employee role name.</param>
+/// <param name="Description">The employee role description.</param>
+public sealed record EmployeeRoleItem(string Id, string Name, string Description);
+
+/// <summary>
+/// The employee role DTO.
+/// </summary>
+public sealed record EmployeeRoleDto 
+{
+    /// <summary>
+    /// Gets or sets the employee role identifier.
+    /// </summary>
+    [XmlAttribute("id")]
+    public string Id { get; init; } = default!;
+
+    /// <summary>
+    /// Gets or sets the employee role name.
+    /// </summary>
+    [XmlAttribute("name")]
+    public string Name { get; init; } = default!;
+
+    /// <summary>
+    /// Gets or sets the employee role description.
+    /// </summary>
+    [XmlAttribute("description")]
+    public string Description { get; init; } = default!;
+}
+
+/// <summary>
+/// The employee roles DTO.
+/// </summary>
+[XmlRoot("employeeRoles")]
+public sealed record EmployeeRolesDto
+{
+    /// <summary>
+    /// Gets or sets the employee roles.
+    /// </summary>
+    [XmlElement("employeeRole")]
+    public List<EmployeeRoleDto> EmployeeRoles { get; init; } = new();
+}
+
+/// <summary>
+/// The employee roles request.
+/// </summary>
+[XmlRoot("employeeRolesRequest")]
+public sealed record EmployeeRolesRequest
+{
+    /// <summary>
+    /// Gets or sets the URL.
+    /// </summary>
+    [XmlElement("url")]
+    public required string Url { get; init; } = default!;
+
+    /// <summary>
+    /// Gets or sets the API token.
+    /// </summary>
+    [XmlElement("apiToken")]
+    public required string ApiToken { get; init; } = default!;
+}
+
+/// <summary>
+/// The employee roles response.
+/// </summary>
+public sealed record EmployeeRolesResponse 
+{
+    /// <summary>
+    /// Gets or sets the result.
+    /// </summary>
+    public bool Result { get; init; }
+
+    /// <summary>
+    /// Gets or sets the employee roles.
+    /// </summary>
+    public IEnumerable<EmployeeRoleItem>? EmployeeRoles { get; init; }
+}
+
+/// <summary>
+/// The employee role item extensions.
+/// </summary>
+internal static class EmployeeRoleItemExtensions 
+{
+    /// <summary>
+    /// Converts the employee role item to the employee role DTO.
+    /// </summary>
+    /// <param name="employeeRoleItem">The employee role item.</param>
+    /// <returns>The employee role DTO.</returns>
+    public static EmployeeRoleDto AsEmployeeRoleDto(this EmployeeRoleItem employeeRoleItem)
+        => new()
+        {
+            Id = employeeRoleItem.Id,
+            Name = employeeRoleItem.Name,
+            Description = employeeRoleItem.Description
+        };
+}
+
+/// <summary>
+/// The employee role items extensions.
+/// </summary>
+internal static class EmployeeRoleItemsExtensions
+{
+    /// <summary>
+    /// Converts the employee role items to the employee roles DTO.
+    /// </summary>
+    /// <param name="employeeRoleItems">The employee role items.</param>
+    /// <returns>The employee roles DTO.</returns>
+    public static EmployeeRolesDto AsEmployeeRolesDto(this IEnumerable<EmployeeRoleItem> employeeRoleItems)
+    {
+        var employeeRolesDto = new EmployeeRolesDto();
+        employeeRolesDto.EmployeeRoles.AddRange(employeeRoleItems.Select(x => x.AsEmployeeRoleDto()));
+        return employeeRolesDto;
+    }
+}
+
+[JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Metadata,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
+[JsonSerializable(typeof(MqIntegrationEvent))]
+[JsonSerializable(typeof(EmployeeRoleItem))]
+[JsonSerializable(typeof(EmployeeRolesResponse))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext { }
+
+public partial class Program { }
---
+++ src/Services/HRLink.EmployeeRole/HRLink.EmployeeRole.FunctionalTests/EmployeeRoleEndpointsTests.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) Alexander Bocharov. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+using Moq;
+using Moq.Protected;
+
+using ParusRx.EventBus.Events;
+using ParusRx.Storage;
+using ParusRx.Xml;
+
+namespace ParusRx.HRLink.EmployeeRole.FunctionalTests;
+
+public class EmployeeRoleEndpointsTests
+{
+    [Fact]
+    public async void GetHealthChecks_ReturnsOk()
+    {
+        // Arrange
+        await using var application = new EmployeeRoleApplication();
+        var client = application.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/health");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async void GetLiveness_ReturnsOk()
+    {
+        // Arrange
+        await using var application = new EmployeeRoleApplication();
+        var client = application.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/liveness");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async void SendEmployeeRolesRequestOnIntegrationEvent_ReturnsCreated()
+    {
+        // Arrange
+        await using var application = new EmployeeRoleApplication();
+        var client = application.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/v1/employeeRoles", new MqIntegrationEvent("XXXXXX"));
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    class EmployeeRoleApplication : WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var employeeRolesRequest = new EmployeeRolesRequest
+            {
+                Url = "https://demo.hr-link.ru",
+                ApiToken = "81255B76-7F18-46E2-93C0-7ED60BE814F9"
+            };
+
+            var employeeRolesResponse = new EmployeeRolesResponse
+            {
+                Result = true,
+                EmployeeRoles = new List<EmployeeRoleItem>
+                {
+                    new EmployeeRoleItem("7CC0C753-0FE9-4153-9EAE-597582292B1B", "Role 1", "Description 1"),
+                    new EmployeeRoleItem("1BB83438-F940-41EA-9E63-806C66CEC566", "Role 2", "Description 2"),
+                    new EmployeeRoleItem("46169F2E-5387-4EF7-94E3-88F9CA6ED643", "Role 3", "Description 3"),
+                    new EmployeeRoleItem("83F431FD-4CAE-4BE4-B34E-D98C2C195D32", "Role 4", "Description 4")
+                }
+            };
+
+            var mockResponse = new HttpResponseMessage()
+            {
+                Content = new StringContent(JsonSerializer.Serialize(employeeRolesResponse)),
+                StatusCode = HttpStatusCode.OK
+            };
+
+            mockResponse.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var httpMessageHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            httpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync", 
+                        ItExpr.IsAny<HttpRequestMessage>(), 
+                        ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(mockResponse)
+                .Verifiable();
+
+            var httpClient = new HttpClient(httpMessageHandler.Object);
+
+            var httpClientFactory = new Mock<IHttpClientFactory>();
+            httpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            var store = new Mock<IParusRxStore>();
+#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+            store.Setup(_ => _.ReadDataRequestAsync(It.IsAny<string>())).ReturnsAsync(XmlSerializerUtility.Serialize(employeeRolesRequest));
+#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+            store.Setup(_ => _.SaveDataResponseAsync(It.IsAny<string>(), It.IsAny<byte[]>())).Returns(Task.CompletedTask);
+
+            builder.ConfigureServices(services =>
+            {
+                services.Replace(ServiceDescriptor.Scoped(_ => store.Object));
+                services.Replace(ServiceDescriptor.Scoped(_ => httpClientFactory.Object));
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
