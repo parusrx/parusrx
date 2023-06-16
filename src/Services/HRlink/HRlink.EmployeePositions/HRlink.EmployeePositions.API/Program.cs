620088b0c9549958625ca0f62e01a74e396283ed
Alexander Bocharov <alex.bocharov@outlook.com>
2023-06-16 10:46:06 +0300

    chore: initial work on hrlink.employee-positions.api

---
+++ src/Services/HRlink/HRlink.EmployeePositions/HRlink.EmployeePositions.API/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Alexander Bocharov. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy());
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions { Predicate = _ => true });
+app.MapHealthChecks("/liveness", new HealthCheckOptions { Predicate = _ => _.Name.Contains("self") });
+
+app.Run();
