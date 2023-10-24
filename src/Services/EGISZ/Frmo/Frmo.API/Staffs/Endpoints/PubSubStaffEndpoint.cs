5d7ee87d9b842a5f663a29c34147e91800b78307
Alexander Bocharov <alex.bocharov@outlook.com>
2023-10-24 09:25:34 +0300

    chore: update Frmo.API.http and PubSubStaffEndpoints

--- src/Services/EGISZ/Frmo/Frmo.API/Staffs/Endpoints/PubSubStaffEndpoint.cs
+++ src/Services/EGISZ/Frmo/Frmo.API/Staffs/Endpoints/PubSubStaffEndpoint.cs
@@ -11,31 +11,31 @@ internal static class PubSubStaffEndpoint
 
         var group = endpoints.MapGroup("api/v1/pubsub/org/staff");
 
-        group.MapPost("/get-all", [Topic(daprPubSubName, "GetStaffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] GetStaffIntegrationEventHandler handler) =>
+        group.MapPost("/", [Topic(daprPubSubName, "FrmoGetStaffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] GetStaffIntegrationEventHandler handler) =>
         {
             await handler.HandleAsync(@event);
             return Results.Ok();
         });
 
-        group.MapPost("/get", [Topic(daprPubSubName, "GetStaffByOidAndEntityIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] GetByEntityStaffIntegrationEventHandler handler) =>
+        group.MapPost("/get", [Topic(daprPubSubName, "FrmoGetStaffByEntityIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] GetByEntityStaffIntegrationEventHandler handler) =>
         {
             await handler.HandleAsync(@event);
             return Results.Ok();
         });
 
-        group.MapPost("/create", [Topic(daprPubSubName, "CreateStafffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] CreateStaffIntegrationEventHandler handler) =>
+        group.MapPost("/create", [Topic(daprPubSubName, "FrmoCreateStafffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] CreateStaffIntegrationEventHandler handler) =>
         {
             await handler.HandleAsync(@event);
             return Results.Ok();
         });
 
-        group.MapPost("/update", [Topic(daprPubSubName, "UpdateStaffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] UpdateDepartmentIntegrationEventHandler handler) =>
+        group.MapPost("/update", [Topic(daprPubSubName, "FrmoUpdateStaffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] UpdateDepartmentIntegrationEventHandler handler) =>
         {
             await handler.HandleAsync(@event);
             return Results.Ok();
         });
 
-        group.MapPost("/delete", [Topic(daprPubSubName, "DeleteStaffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] DeleteDepartmentIntegrationEventHandler handler) =>
+        group.MapPost("/delete", [Topic(daprPubSubName, "FrmoDeleteStaffIntegrationEvent")] async ([FromBody] MqIntegrationEvent @event, [FromServices] DeleteDepartmentIntegrationEventHandler handler) =>
         {
             await handler.HandleAsync(@event);
             return Results.Ok();
