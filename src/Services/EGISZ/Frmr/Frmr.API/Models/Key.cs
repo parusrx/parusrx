1b4f4faa695140d531e5d7ade7d7d3e613a88c96
Alexander Bocharov <alex.bocharov@outlook.com>
2023-10-24 15:59:39 +0300

    chore: add models for person accreditation

---
+++ src/Services/EGISZ/Frmr/Frmr.API/Models/Key.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) The Parus RX Authors. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace ParusRx.Frmr.API;
+
+public record Key
+{
+    [XmlElement("snils")]
+    [JsonPropertyName("snils")]
+    public string? Snils { get; init; }
+
+    [XmlElement("document")]
+    [JsonPropertyName("document")]
+    public Document? Document { get; init; }
+}
