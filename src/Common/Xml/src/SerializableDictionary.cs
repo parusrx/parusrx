919a2b910e9b0e428646c6e5310b9207f812bee6
Alexander Bocharov <alex.bocharov@outlook.com>
2023-10-30 14:44:16 +0300

    chore: add SerializableDictionary implementation

---
+++ src/Common/Xml/src/SerializableDictionary.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) The Parus RX Authors. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ParusRx.Xml;
+
+public sealed class SerializableDictionary : Dictionary<string, string?>, IXmlSerializable
+{
+    /// <inheritdoc/>
+    public XmlSchema? GetSchema() => null;
+
+    /// <inheritdoc/>
+    public void ReadXml(XmlReader reader)
+    {
+        var wasEmpty = reader.IsEmptyElement;
+        reader.Read();
+
+        if (wasEmpty)
+        {
+            return;
+        }
+
+        while (reader.NodeType != XmlNodeType.EndElement)
+        {
+            if (reader.NodeType != XmlNodeType.Element)
+            {
+                reader.Read();
+                continue;
+            }
+            // if (reader.NodeType == XmlNodeType.Element)
+            // {
+                string key = reader.LocalName;
+                string? value = reader.ReadElementContentAsString();
+
+                Add(key, value);
+            // }
+        }
+
+        reader.ReadEndElement();
+    }
+
+    /// <inheritdoc/>
+    public void WriteXml(XmlWriter writer)
+    {
+        foreach(var kvp in this.Where(kvp => kvp.Value is not null))
+        {
+            writer.WriteElementString(kvp.Key, kvp.Value);
+        }
+    }
+}
---
+++ src/Common/Xml/test/SerializableDictionaryTest.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) The Parus RX Authors. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ParusRx.Xml.Tests;
+
+public class SerializableDictionaryTest
+{
+    [Fact]
+    public void CanSeriaLize()
+    {
+        // Arrange
+        string xml = $"""
+            <?xml version="1.0" encoding="utf-16"?>
+            <SerializableDictionary>
+              <key1>value1</key1>
+              <key2>value2</key2>
+            </SerializableDictionary>
+            """;
+
+        var dictionary = new SerializableDictionary
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+            { "key3", null }
+        };
+        var serializer = new XmlSerializer(typeof(SerializableDictionary));
+        var writer = new StringWriter();
+
+        // Act
+        serializer.Serialize(writer, dictionary);
+
+        // Assert
+        Assert.Equal(writer.ToString(), xml); 
+   }
+
+    [Fact]
+    public void TestDeserialization()
+    {
+        // Arrange
+        string xml = $"""
+            <?xml version="1.0" encoding="utf-16"?>
+            <SerializableDictionary>
+              <key1>value1</key1>
+              <key2>value2</key2>
+            </SerializableDictionary>
+            """;
+        var serializer = new XmlSerializer(typeof(SerializableDictionary));
+        var reader = XmlReader.Create(new StringReader(xml));
+
+        // Act
+        var dictionary = (SerializableDictionary?)serializer.Deserialize(reader);
+
+        // Assert
+        Assert.NotNull(dictionary);
+        Assert.Equal("value1", dictionary["key1"]);
+        Assert.Equal("value2", dictionary["key2"]);
+    }
+}
