99b889086c67131a3380fd0ac4e0ebedfec2d9bf
Alexander Bocharov <alex.bocharov@outlook.com>
2023-11-23 15:45:48 +0300

    chore (mis): add an API for receiving education certificate and accreditation

--- src/Services/Egisz/Mis/Mis.API/Apis/PersonApi.cs
+++ src/Services/Egisz/Mis/Mis.API/Apis/PersonApi.cs
@@ -8,11 +8,31 @@ public static class PersonApi
     public static IEndpointRouteBuilder MapPersonApi(this IEndpointRouteBuilder app)
     {
         app.MapGet("/full", GetFullPerson);
+        app.MapGet("/cert", ListEducationCert);
+        app.MapGet("/accreditation", GetPersonAccreditation);
 
         return app;
     }
 
     public static async ValueTask<Ok<SingleResponse<FullPerson>>> GetFullPerson(HttpRequest request, IFullPersonService service, CancellationToken cancellationToken)
+    {
+        var queryParameters = request.Query.ToDictionary(x => x.Key, x => (string?)x.Value.ToString());
+
+        var response = await service.GetAsync(queryParameters, cancellationToken);
+
+        return TypedResults.Ok(response);
+    }
+
+    public static async ValueTask<Ok<ListResponse<EducationCert>>> ListEducationCert(HttpRequest request, IEducationCertService service, CancellationToken cancellationToken)
+    {
+        var queryParameters = request.Query.ToDictionary(x => x.Key, x => (string?)x.Value.ToString());
+
+        var response = await service.ListAsync(queryParameters, cancellationToken);
+
+        return TypedResults.Ok(response);
+    }
+
+    public static async ValueTask<Ok<SingleResponse<PersonAccreditation>>> GetPersonAccreditation(HttpRequest request, IPersonAccreditationService service, CancellationToken cancellationToken)
     {
         var queryParameters = request.Query.ToDictionary(x => x.Key, x => (string?)x.Value.ToString());
 
