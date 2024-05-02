ec2315dc1c8402c34ed9c818466ac0bd8158e593
Alexander Bocharov <alex.bocharov@outlook.com>
2024-05-02 23:59:07 +0300

    `Added JsonStringEnumMemberConverter<T> for JSON-Enum conversion`
    Implemented a new class `JsonStringEnumMemberConverter<T>` that inherits from `JsonConverter<T>`. This class is designed to convert JSON strings to Enum and vice versa. The class includes two main methods: `Read` and `Write`. The `Read` method converts a JSON string to an Enum, throwing a `JsonException` for invalid inputs or if no matching Enum member is found. The `Write` method converts an Enum to a JSON string, also throwing a `JsonException` if no matching Enum member is found or if the `EnumMemberAttribute` is null.

---
+++ src/Services/Astral/Astral.Domain/Json/JsonStringEnumMemberConverter.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Alexander Bocharov.
+// Licensed under the MIT License. See the LICENSE file in the project root for more information.
+
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace ParusRx.Astral.Domain.Json;
+
+public class JsonStringEnumMemberConverter<T> : JsonConverter<T> where T : Enum
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+        {
+            throw new JsonException();
+        }
+
+        var enumValue = reader.GetString();
+        if (enumValue == null)
+        {
+            throw new JsonException();
+        }
+
+        var enumType = typeof(T);
+        var member = enumType.GetMembers().FirstOrDefault(m => m.GetCustomAttribute<EnumMemberAttribute>()?.Value == enumValue);
+        if (member == null)
+        {
+            throw new JsonException();
+        }
+
+        return (T)Enum.Parse(enumType, member.Name);
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        var enumType = typeof(T);
+        var member = enumType.GetMember(value.ToString()).FirstOrDefault();
+        if (member == null)
+        {
+            throw new JsonException();
+        }
+
+        var enumMember = member.GetCustomAttribute<EnumMemberAttribute>();
+        if (enumMember == null)
+        {
+            throw new JsonException();
+        }
+
+        writer.WriteStringValue(enumMember.Value);
+    }
+}
