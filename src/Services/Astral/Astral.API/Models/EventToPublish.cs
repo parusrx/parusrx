00a9160dd767aef3b805f7634b0e7e75adb42ce1
Alexander Bocharov <alex.bocharov@outlook.com>
2024-05-03 00:01:03 +0300

    Updated EventToPublish.cs with new namespaces and attributes
    Updated the `EventToPublish.cs` file with several significant changes. Added two new `using` directives: `System.Runtime.Serialization` and `ParusRx.Astral.Domain.Poa` for easier type usage. Introduced the `ParusRx.Astral.API.Models` namespace. Added four new attributes to the `EventToPublish` record for serialization and deserialization: `XmlInclude` and `KnownType` for both `PoaCreateUnifiedRequest` and `PoaCreateUnifiedResponse`. These changes enhance the serialization/deserialization process of the `EventToPublish` object.

--- src/Services/Astral/Astral.API/Models/EventToPublish.cs
+++ src/Services/Astral/Astral.API/Models/EventToPublish.cs
@@ -1,8 +1,16 @@
 ﻿// Copyright (c) Alexander Bocharov.
 // Licensed under the MIT License. See the LICENSE file in the project root for more information.
 
+using System.Runtime.Serialization;
+
+using ParusRx.Astral.Domain.Poa;
+
 namespace ParusRx.Astral.API.Models;
 
+[XmlInclude(typeof(PoaCreateUnifiedRequest))]
+[XmlInclude(typeof(PoaCreateUnifiedResponse))]
+[KnownType(typeof(PoaCreateUnifiedRequest))]
+[KnownType(typeof(PoaCreateUnifiedResponse))]
 public record EventToPublish
 {
     [XmlElement("code")]
