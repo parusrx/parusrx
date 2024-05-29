2f0633938688e931c0148b4c7ad802e4c13be44d
Alexander Bocharov <alex.bocharov@outlook.com>
2024-05-29 11:31:19 +0300

    Updated Docker settings and downgraded health check package
    Updated `docker-compose.yml` to run the application in a production environment. The base image in `Dockerfile` was updated to use the newer version of the .NET runtime. The `HEALTHCHECK` command in `Dockerfile` was uncommented for Docker to periodically check the application's health. Downgraded the `AspNetCore.HealthChecks.UI.Client` package in `Ips.IdentityProvider.API.csproj` to an older version. Added the `HealthChecks.UI.Client` namespace in `Program.cs` for easier access to its classes and methods.

--- src/Services/Egisz/Ips/Ips.IdentityProvider.API/Program.cs
+++ src/Services/Egisz/Ips/Ips.IdentityProvider.API/Program.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography.Pkcs;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json.Serialization;
+
 using HealthChecks.UI.Client;
 
 using LibCore.Security.Cryptography.X509Certificates;
