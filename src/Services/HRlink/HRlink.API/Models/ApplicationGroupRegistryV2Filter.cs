4623a052af13341331fb9288dfdb1584e1253858
Alexander Bocharov <alex.bocharov@outlook.com>
2025-06-16 15:23:00 +0300

    Add JsonIgnore attributes to filter properties
    This commit adds the `[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]` attribute to multiple properties in the `ApplicationGroupRegistryV2Filter` record to prevent null values from being serialized in JSON. The affected properties include `ApplicationTypeIds`, `ApplicationDateFrom`, `ApplicationDateTo`, `ApplicationCreatedDateFrom`, `ApplicationCreatedDateTo`, `ApplicationDocflowFinishedDateFrom`, `ApplicationDocflowFinishedDateTo`, `ApplicationEventDateFrom`, `ApplicationEventDateTo`, `EmployeeIds`, `LegalEntityIds`, `ClientDepartmentIds`, `Limit`, `Offset`, `ApplicationIds`, `ApplicationStatuses`, `ClientUserParticipantIds`, and `ClientUserResponsibleIds`. The `Limit` and `Offset` properties are also initialized with default values of `3` and `0`, respectively.

--- src/Services/HRlink/HRlink.API/Models/ApplicationGroupRegistryV2Filter.cs
+++ src/Services/HRlink/HRlink.API/Models/ApplicationGroupRegistryV2Filter.cs
@@ -8,81 +8,99 @@ public record ApplicationGroupRegistryV2Filter
     [XmlArray("applicationTypeIds")]
     [XmlArrayItem("applicationTypeIdsItem")]
     [JsonPropertyName("applicationTypeIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? ApplicationTypeIds { get; init; }
 
     [XmlElement("applicationDateFrom")]
     [JsonPropertyName("applicationDateFrom")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationDateFrom { get; init; }
 
     [XmlElement("applicationDateTo")]
     [JsonPropertyName("applicationDateTo")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationDateTo { get; init; }
 
     [XmlElement("applicationCreatedDateFrom")]
     [JsonPropertyName("applicationCreatedDateFrom")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationCreatedDateFrom { get; init; }
 
     [XmlElement("applicationCreatedDateTo")]
     [JsonPropertyName("applicationCreatedDateTo")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationCreatedDateTo { get; init; }
 
     [XmlElement("applicationDocflowFinishedDateFrom")]
     [JsonPropertyName("applicationDocflowFinishedDateFrom")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationDocflowFinishedDateFrom { get; init; }
 
     [XmlElement("applicationDocflowFinishedDateTo")]
     [JsonPropertyName("applicationDocflowFinishedDateTo")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationDocflowFinishedDateTo { get; init; }
 
     [XmlElement("applicationEventDateFrom")]
     [JsonPropertyName("applicationEventDateFrom")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationEventDateFrom { get; init; }
 
     [XmlElement("applicationEventDateTo")]
     [JsonPropertyName("applicationEventDateTo")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ApplicationEventDateTo { get; init; }
 
     [XmlArray("employeeIds")]
     [XmlArrayItem("employeeIdsItem")]
     [JsonPropertyName("employeeIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? EmployeeIds { get; init; }
 
     [XmlArray("legalEntityIds")]
     [XmlArrayItem("legalEntityIdsItem")]
     [JsonPropertyName("legalEntityIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? LegalEntityIds { get; init; }
 
     [XmlArray("clientDepartmentIds")]
     [XmlArrayItem("clientDepartmentIdsItem")]
     [JsonPropertyName("clientDepartmentIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? ClientDepartmentIds { get; init; }
 
     [XmlElement("limit")]
     [JsonPropertyName("limit")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int Limit { get; init; } = 3;
 
     [XmlElement("offset")]
     [JsonPropertyName("offset")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int Offset { get; init; } = 0;
 
     [XmlArray("applicationIds")]
     [XmlArrayItem("applicationIdsItem")]
     [JsonPropertyName("applicationIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? ApplicationIds { get; init; }
 
     [XmlArray("applicationStatuses")]
     [XmlArrayItem("applicationStatusesItem")]
     [JsonPropertyName("applicationStatuses")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? ApplicationStatuses { get; init; }
 
     [XmlArray("clientUserParticipantIds")]
     [XmlArrayItem("clientUserParticipantIdsItem")]
     [JsonPropertyName("clientUserParticipantIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? ClientUserParticipantIds { get; init; }
 
     [XmlArray("clientUserResponsibleIds")]
     [XmlArrayItem("clientUserResponsibleIdsItem")]
     [JsonPropertyName("clientUserResponsibleIds")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string[]? ClientUserResponsibleIds { get; init; }
 
     [XmlElement("onlyEmployeeApplications")]
